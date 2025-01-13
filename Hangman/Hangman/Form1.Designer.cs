38d6a30c8cbd691646314c1b8308d2c7de42d97a
Ibrahimefe2 <147322544+Ibrahimefe2@users.noreply.github.com>
2025-01-13 13:50:27 +0300

    Add files via upload

---
+++ Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKelime = new System.Windows.Forms.Label();
+            this.txtTahmin = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblKalanHak = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblKelime
+            // 
+            this.lblKelime.AutoSize = true;
+            this.lblKelime.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKelime.Location = new System.Drawing.Point(40, 70);
+            this.lblKelime.Name = "lblKelime";
+            this.lblKelime.Size = new System.Drawing.Size(0, 26);
+            this.lblKelime.TabIndex = 0;
+            this.lblKelime.Click += new System.EventHandler(this.lblKelime_Click);
+            // 
+            // txtTahmin
+            // 
+            this.txtTahmin.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTahmin.Location = new System.Drawing.Point(45, 215);
+            this.txtTahmin.Name = "txtTahmin";
+            this.txtTahmin.Size = new System.Drawing.Size(100, 34);
+            this.txtTahmin.TabIndex = 1;
+            this.txtTahmin.TextChanged += new System.EventHandler(this.txtTahmin_TextChanged);
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(167)))), ((int)(((byte)(69)))));
+            this.btnGiris.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnGiris.FlatAppearance.BorderSize = 0;
+            this.btnGiris.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGiris.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGiris.Location = new System.Drawing.Point(240, 215);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(100, 34);
+            this.btnGiris.TabIndex = 2;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hangman.Properties.Resources.adam;
+            this.pictureBox1.Location = new System.Drawing.Point(435, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 250);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblKalanHak
+            // 
+            this.lblKalanHak.AutoSize = true;
+            this.lblKalanHak.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKalanHak.Location = new System.Drawing.Point(430, 276);
+            this.lblKalanHak.Name = "lblKalanHak";
+            this.lblKalanHak.Size = new System.Drawing.Size(133, 26);
+            this.lblKalanHak.TabIndex = 4;
+            this.lblKalanHak.Text = "Kalan Hak:";
+            this.lblKalanHak.Click += new System.EventHandler(this.lblKalanHak_Click);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(40, 365);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(0, 26);
+            this.lblDurum.TabIndex = 5;
+            this.lblDurum.Click += new System.EventHandler(this.lblDurum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblKalanHak);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtTahmin);
+            this.Controls.Add(this.lblKelime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKelime;
+        private System.Windows.Forms.TextBox txtTahmin;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblKalanHak;
+        private System.Windows.Forms.Label lblDurum;
+    }
+}
+
---
+++ Hangman/Hangman/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        private string[] sehirler =  {"Adana", "Adiyaman", "Afyonkarahisar", "Agri", "Aksaray", "Amasya", "Ankara",
+            "Antalya", "Ardahan", "Artvin", "Aydin", "Balikesir", "Bartin", "Batman",
+            "Bayburt", "Bilecik", "Bingol", "Bitlis", "Bolu", "Burdur", "Bursa", "Canakkale",
+            "Cankiri", "Corum", "Denizli", "Diyarbakir", "Duzce", "Edirne", "Elazig", "Erzincan",
+            "Erzurum", "Eskisehir", "Gaziantep", "Giresun", "Gumushane", "Hakkari", "Hatay",
+            "Igdir", "Isparta", "Istanbul", "Izmir", "Kahramanmaras", "Karabuk", "Karaman",
+            "Kars", "Kastamonu", "Kayseri", "Kirikale", "Kirklareli", "Kirsehir", "Kilis",
+            "Kocaeli", "Konya", "Kutahya", "Malatya", "Manisa", "Mardin", "Mersin", "Mugla",
+            "Mus", "Nevsehir", "Nigde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun",
+            "Siirt", "Sinop", "Sivas", "Sanliurfa", "Sirnak", "Tekirdag", "Tokat", "Trabzon",
+            "Tunceli", "Usak", "Van", "Yalova", "Yozgat", "Zonguldak"};
+        // sehirlerin isimlerini turkce karakter olmadan diziye tanimladik.
+
+        private string secilenSehir; 
+        private int kalanHak = 6;
+        private char[] dogruHarfler;
+        private string acilmamisSehir;
+        //Programda ilerde isimize yarayacak degiskenleri tanimladik.
+
+        //Oyunu baslatacak olan metot. 
+        private void oyunuBaslat()
+        {
+            Random rnd = new Random();
+            int rastgeleSayi = rnd.Next(0, 81);
+            //Rastgele bir sayi seciliyor bu sayi sehrin rastgele secilmesini saglayacak.
+
+            secilenSehir = sehirler[rastgeleSayi].ToUpper();
+            //rastgele secilen sehiri ilerde kontrol edebilmek icin secilenSehir degiskenine atadık.
+
+            dogruHarfler = secilenSehir.ToCharArray();
+            //secilen sehrin harflerini bir diziye atadik bu sayede kullanicinin girdigi harfin kontrolunu yapabilecegiz.
+
+            for (int i = 0; i < dogruHarfler.Length; i++)
+            {
+                lblKelime.Text += "_ ";  
+            }
+            //secilen sehir kac harfli ise o kadar _ atacak dongu blogu.
+
+            lblKalanHak.Text = "Kalan Hak: " + kalanHak;
+            lblDurum.Text = "";
+            pictureBox1.Image = Properties.Resources.adam;
+            //oyun basinda kalan hak, oyun durumu ve fotografi sifirladik.
+
+        }
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            oyunuBaslat();
+            //metotu kullanmak icin form1 altinda cagirdik.
+        }
+
+        //butona basilinca yapilacak islemler.
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            string tahmin = txtTahmin.Text.ToUpper();
+            bool dogruTahmin = false;
+            //kullanicinin tahmini buyuk harfe cevirip tahmin adli degiskene atadik.
+            //tahminin dogrulugunun kontrolu icin dogruTahmin adinda boolean bir degisken tanimladik.
+
+            StringBuilder yeniKelime = new StringBuilder(lblKelime.Text.Replace(" ", "")); 
+            //kelimenin acilmamis halini dogru harf geldikce degisterecegimiz icin stringBuilder'dan yararlaniyoruz.
+
+            //kullanicinin girdigi harfin kelime icinde olup olmadigini kontrol edecegimiz dongu.
+            for (int i = 0; i < dogruHarfler.Length; i++)
+            {
+                //eger harf kelimenin icinde ise burdaki if blogu calisir.
+                if (dogruHarfler[i].Equals(tahmin[0]))
+                {
+                    yeniKelime[i] = tahmin[0];  
+                    //harf dogru ise _ 'nın yerine gececek.
+
+                    dogruTahmin = true;
+                    //dogruTahmin adli degiskeni true olarak ayarladik.
+                }
+            }
+
+            lblKelime.Text = string.Join(" ", yeniKelime.ToString().ToCharArray()); 
+            //kelimenin gizli halinin oldugu labeli dogru harf yerine yazilmis sekilde guncelledik.
+
+            //eger tahmin dogru degilse bu if blogu calisacak.
+            if (!dogruTahmin)
+            {
+                kalanHak--; 
+                //kalan Hak 1 azaltildi.
+
+                int j = 6 - kalanHak;
+                string resourceName = $"adam_{j}";
+                var image = (System.Drawing.Image)Properties.Resources.ResourceManager.GetObject(resourceName);
+                pictureBox1.Image = image;
+                //Burada pictureBox'taki adam asmaca fotografini guncelledik. Bunu yaparken j diye bir degisken tanimladik.
+                //Bu degisken dosyalarda saklanan adam_1, adam_2 ...  gibi dosyalari cagirirken pratiklik saglayacaktir.
+                //Dogru yerden baslamak icinde 6'dan kalan hakki cikararak j'ye atadik
+                
+            }
+            lblKalanHak.Text = "Kalan Hak: " + kalanHak;
+            //Kullanicinin guncel kalan hakkinin yazdigi labeli guncelledik.
+
+            //Oyunun durumu yani kazanip kaybetme durumunun kontrolu bu if blogunda incelenecek.
+            if (!lblKelime.Text.Contains("_"))
+                //eger kelimenin yazdigi labelde _ kalmadiysa kullanici kazanmis olacaktir.
+            {
+                lblDurum.Text = "Tebrikler, Kazandınız!";
+                lblDurum.ForeColor = Color.Green;
+                btnGiris.Enabled = false;
+                //durumun yazdigi labeli guncelledik ve butonu deaktif hale getirdik.
+            }
+            if (kalanHak <= 0)
+                // eger kullanicinin hakki kalmadiysa kullanici kaybetmis olacaktir.
+            {
+                lblDurum.Text = "Kaybettiniz! Doğru cevap: " + secilenSehir;
+                lblDurum.ForeColor = Color.Red;
+                btnGiris.Enabled = false;
+                //durumun yazdigi labeli guncelledik ve butonu deaktif hale getirdik.
+            }
+        }
+
+        private void lblKalanHak_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDurum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTahmin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblKelime_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
